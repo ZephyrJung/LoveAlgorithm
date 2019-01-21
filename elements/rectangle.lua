f3dc3467ace549a297f6484074ef5f9069c59e46
张煜 <zephyrjung@126.com>
2019-01-21 08:57:53 +0800

    add color rectangles

---
+++ elements/rectangle.lua
@@ -0,0 +1,3 @@
+function defaultDraw(startX, startY, height)
+    love.graphics.rectangle("fill", startX, startY - height * 10, 10, height * 10 )
+end
--- main.lua
+++ main.lua
@@ -1,3 +1,33 @@
-function love.draw()
-    love.graphics.print("Hello World", 400, 300)
+require "elements/rectangle"
+require "elements/color"
+Color = require('lib/ColorLibrary')
+
+function love.load()
+    size = 0
+    text = "hello "
+    rectangles = {}
 end
+
+function love.draw()
+    i = 0
+    for k,v in pairs(rectangles)
+    do
+        love.graphics.setColor(v[4],v[5],v[6])
+        defaultDraw(v[1],v[2],v[3])
+        love.graphics.setColor(Color("white"))
+        love.graphics.print(text, 400, 250)
+        i = i+1
+    end
+end
+
+function love.keyreleased(key)
+    if key == "return" then
+       size = size + 1
+       r,g,b = colorList(size)
+       text = "hello "..size.." "..r..","..g..","..b
+       x = 10 + 15 * math.fmod((size - 1),50)
+       y = 80 + 85 * math.modf((size - 1) / 50 )
+       h = 5
+       table.insert(rectangles,{x,y,h,r,g,b})
+    end
+ end
